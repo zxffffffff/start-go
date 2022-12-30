9095b3a199319da3e90cb04e96296daf5ee4e3f2
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2022-12-30 13:42:20 +0800

    add main & datacenter

---
+++ sample-main/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	datacenter "github.com/zxffffffff/start-go/sample-datacenter"
+)
+
+func printMsg(key string) {
+	msg, err := datacenter.GetMsg(key)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(msg)
+	}
+}
+
+func main() {
+	log.SetPrefix("[sample-main]: ")
+	log.SetFlags(0)
+
+	printMsg("test")
+	printMsg("")
+}
